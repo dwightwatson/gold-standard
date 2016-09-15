f22111f0352091b4fef80deb7a6b44eb347e77f1
Dwight Watson <dwightwatson@me.com>
2016-09-16 00:13:37 +1000

    Do some work on example tests

--- tests/PostsTest.php
+++ tests/PostsTest.php
@@ -1,6 +1,34 @@
 <?php
 
+use App\Post;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 class PostsTest extends \TestCase
 {
-    //
+    use DatabaseTransactions;
+
+    /** @test */
+    function it_displays_the_index_page()
+    {
+        $post = factory(Post::class)->create();
+
+        $this->visit('/posts')
+            ->see($post->title);
+    }
+
+    /** @test */
+    function it_displays_the_create_form()
+    {
+        $this->visit('posts/create');
+    }
+
+    /** @test */
+    function it_stores_a_valid_post()
+    {
+        $attributes = factory(Post::class)->build()->toArray();
+
+        $this->visit('/posts/create');
+
+        //
+    }
 }
